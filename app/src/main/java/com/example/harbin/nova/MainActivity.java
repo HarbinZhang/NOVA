af84ca01a21633c7192d9efe11afdc68f1ecdbc1
HarbinZhang <seashore.real@gmail.com>
2017-05-09 23:03:52 -0400

    2nd

--- app/src/main/java/com/example/harbin/nova/MainActivity.java
+++ app/src/main/java/com/example/harbin/nova/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.harbin.nova;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void set_reminder_click(View view){
+        Intent start_set_reminder = new Intent(this, SetReminder.class);
+        startActivity(start_set_reminder);
     }
 }
---
+++ app/src/main/java/com/example/harbin/nova/SetReminder.java
@@ -0,0 +1,20 @@
+package com.example.harbin.nova;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+
+public class SetReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_reminder);
+
+        RecyclerView reminderRecyclerView;
+
+
+
+    }
+}
