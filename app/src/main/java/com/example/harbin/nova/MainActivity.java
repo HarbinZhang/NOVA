11fe1060537f184b7b76c790e920ba48f31ffc2f
HarbinZhang <seashore.real@gmail.com>
2017-05-09 22:54:36 -0400

    first commit

---
+++ app/src/main/java/com/example/harbin/nova/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.harbin.nova;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
